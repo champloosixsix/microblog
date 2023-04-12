778936a7337ff2ffaa840b02d4465d1475d65bb7
champloosixsix <seanlovins@gmail.com>
2023-04-12 17:45:46 -0400

    added error handling

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,4 +12,4 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
